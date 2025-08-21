82bcdfa7bfdc9904067a20d1f4c0400a8229ca36
kmj5276 <myeongjae1235@gmail.com>
2025-08-21 17:44:59 +0900

    4단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("==명언 앱==");
+        int no = 1;
 
         while(true) {
             System.out.print("명령) ");
@@ -18,7 +19,8 @@ public class Main {
                 String wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(no + "번 명언이 등록되었습니다.");
+                no++;
             }
         }
     }
