ef1f9f9590e27ae2258096bfc55a955a557cf99b
kmj5276 <myeongjae1235@gmail.com>
2025-08-21 17:39:14 +0900

    3단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -18,6 +18,7 @@ public class Main {
                 String wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
