a9b877250c55be95fef1c432d7b1ce4eadbf638e
kmj5276 <myeongjae1235@gmail.com>
2025-09-10 17:44:53 +0900

    리펙토링, WiseSaying 클래스에 전체 필드를 초기화 하는 생성자 도입

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -51,10 +51,10 @@ public class App {
 
         WiseSaying modifyTargetWiseSaying = wiseSayings[modifyTargetIndex];
 
-        System.out.println("명언(기존) : %s".formatted(modifyTargetWiseSaying.saying));
+        System.out.println("명언(기존) : %s".formatted(modifyTargetWiseSaying.getSaying()));
         System.out.print("명언 : ");
         String newSaying = sc.nextLine();
-        System.out.println("작가(기존) : %s".formatted(modifyTargetWiseSaying.author));
+        System.out.println("작가(기존) : %s".formatted(modifyTargetWiseSaying.getAuthor()));
         System.out.print("작가 : ");
         String newAuthor = sc.nextLine();
 
@@ -63,8 +63,8 @@ public class App {
     }
 
     public void modify(WiseSaying modifyTargetWiseSaying, String newSaying, String newAuthor) {
-        modifyTargetWiseSaying.saying = newSaying;
-        modifyTargetWiseSaying.author = newAuthor;
+        modifyTargetWiseSaying.setSaying(newSaying);
+        modifyTargetWiseSaying.setAuthor(newAuthor);
     }
 
     public void actionDelete(String command) {
@@ -90,7 +90,7 @@ public class App {
 
     public int findIndexById(int id) {
         for(int i = 0; i < lastIndex; i++) {
-            if(wiseSayings[i].id == id){
+            if(wiseSayings[i].getId() == id){
                 return i;
             }
         }
@@ -121,7 +121,7 @@ public class App {
         WiseSaying[] wiseSayings = wiseSayingListDesc();
 
         for (WiseSaying wiseSaying : wiseSayings) {
-            System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.saying));
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getAuthor()));
         }
     }
 
@@ -146,15 +146,12 @@ public class App {
 
         WiseSaying wiseSaying = write(saying, author);
 
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
     public WiseSaying write(String saying, String author) {
         lastNo++;
 
-        WiseSaying wiseSaying = new WiseSaying();
-        wiseSaying.id = lastNo;
-        wiseSaying.saying = saying;
-        wiseSaying.author = author;
+        WiseSaying wiseSaying = new WiseSaying(lastNo, saying, author);
 
         wiseSayings[lastIndex++] = wiseSaying;
 
