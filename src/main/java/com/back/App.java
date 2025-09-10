6f25b8b2159de5f230cfb8757fe1359629cb9f3b
kmj5276 <myeongjae1235@gmail.com>
2025-09-10 09:24:19 +0900

    요구사항 6단계 구현, 배열에서 특정 index 삭제하는 로직 구현

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -19,24 +19,61 @@ public class App {
 
             if(command.equals("종료")){
                 break;
-            } else if (command.equals("등록")) {
+            } else if(command.equals("등록")) {
                 actionWrite();
             } else if(command.equals("목록")){
                 actionList();
-                                }
+            } else if(command.equals("삭제?id=1")){
+                actionDelete();
             }
         }
+    }
+
+    public void actionDelete() {
+        delete();
+        System.out.println("1번 명언이 삭제되었습니다.");
+    }
+
+    public void delete() {
+        int deleteTargetIndex = -1; // 삭제하고 싶은 명언이 저장될 위치
+
+        for(int i = 0; i < lastIndex; i++) {
+            if(wiseSayings[i].id == 1){
+                deleteTargetIndex = i;
+            }
+        }
+
+        for(int i = deleteTargetIndex; i < lastIndex; i++){
+            wiseSayings[i] = wiseSayings[i + 1];
+        }
+
+        lastIndex --;
+    }
 
     public void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-        for (int i = lastIndex - 1; i >= 0; i--) {
-            WiseSaying target = wiseSayings[i];
+        WiseSaying[] wiseSayings = wiseSayingListDesc();
 
-            System.out.println("%d / %s / %s".formatted(target.id, target.author, target.saying));
+        for (WiseSaying wiseSaying : wiseSayings) {
+            System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.saying));
         }
     }
+
+    public WiseSaying[] wiseSayingListDesc() {
+
+        WiseSaying[] descList = new WiseSaying[lastIndex];
+        int descListIndex = 0;
+
+        for(int i = lastIndex - 1; i >= 0; i--){
+            descList[descListIndex] = wiseSayings[i];
+            descListIndex++;
+        }
+
+        return descList;
+    }
+
     public void actionWrite () {
         System.out.print("명언 : ");
         String saying = sc.nextLine();
